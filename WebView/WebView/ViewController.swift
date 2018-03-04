2ad57f6210cfd4a3c4abc21573a218afa2095978
Zewen Wang <w.zewen@outlook.com>
2018-03-04 15:38:24 -0500

    open web page within the app or in safari

--- WebView/WebView/ViewController.swift
+++ WebView/WebView/ViewController.swift
@@ -5,21 +5,38 @@
 //  Created by Wang, Zewen on 2018-03-04.
 //  Copyright © 2018 Wang, Zewen. All rights reserved.
 //
-
 import UIKit
-
-class ViewController: UIViewController {
-
+import WebKit
+class ViewController: UIViewController,  WKUIDelegate, WKNavigationDelegate {
+    let urlString = "https://www.google.com"
+    // The protocol has to be https
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        // make url(String Type) to URL, and make URL to URLRequest
+        let url = URL(string: urlString)
+        let request = URLRequest(url: url!)
+        
+        //open the web page within this app
+        webView.load(request)
+        
+        //open web page in safari
+        //UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+        
+     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        //SVProgressHUD.show()
     }
-
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        DispatchQueue.main.async{
+           // SVProgressHUD.dismiss()
+        }
+    }
 
 }
 
